6f7ffb4fb336a88605cd58aac0fbb31744013b3b
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-27 11:29:38 -0300

    feat:Criando e estilizando o card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Card from "./routes/Card" 
+import Home from "./routes/Home"
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/card" element={<Card/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
